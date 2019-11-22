f38d696e2a6167fe0410f0c2e578fcee6f47490d
ebenezum <ebenezum@o2.pl>
2019-11-23 00:02:12 +0100

    adding function for placing first sign

--- app/board.py
+++ app/board.py
@@ -1,6 +1,10 @@
 class Board():
     def __init__(self):
+        self.matrix = []
+    
+    def create(self):
         self.matrix = [['-' for var_x in range(3)] for var_y in range(3)]
+        return self.matrix
     
     def show(self):
         """ Printing board list as picture"""
--- app/main.py
+++ app/main.py
@@ -1,7 +1,24 @@
 import board as brd
+import random as rnd
 
 print("Let's start here")
-
 newboard = brd.Board()
+newboard.create()
 
-print(newboard.show())
+vstee = 0
+# todo: random player selection
+vplayer = 'A'
+
+while vstee < 8:
+    if vstee == 0:
+        vxp = rnd.randint(0,2)
+        vyp = rnd.randint(0,2)
+        vsymbol = 'X' if vplayer == 'A' else 'O'
+        newboard[vxp][vyp] = vsymbol
+        print(newboard.show())
+    else:
+        print("Tu bedzie else")
+    print(vstee)
+    vstee += 1
+
+
