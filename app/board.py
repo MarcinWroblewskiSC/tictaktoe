af35884917f639691a87fe4cb821f681898aeb0c
ebenezum <ebenezum@o2.pl>
2019-11-24 20:18:18 +0100

    add first strategy

--- app/board.py
+++ app/board.py
@@ -1,11 +1,14 @@
 class Board():
     def __init__(self):
         self.matrix = []
-    
+
     def create(self):
-        self.matrix = [['-' for var_x in range(3)] for var_y in range(3)]
+        self.matrix = [[0 for var_x in range(3)] for var_y in range(3)]
         return self.matrix
-    
+
+    def __getitem__(self, keyx):
+        return self.matrix[keyx]
+
     def show(self):
         """ Printing board list as picture"""
         rez = ""
@@ -13,7 +16,11 @@ class Board():
             rez += str(cell) + "\n"
         return rez
 
+    def showFlat(self):
+        rez = self.matrix
+        return rez
+
     def board_cls(self):
         """Method to clear current board"""
         self.matrix = None
-        self.matrix = [['-' for var_x in range(3)] for var_y in range(3)]
+        self.matrix = [[0 for var_x in range(3)] for var_y in range(3)]
--- app/main.py
+++ app/main.py
@@ -2,23 +2,43 @@ import board as brd
 import random as rnd
 
 print("Let's start here")
-newboard = brd.Board()
-newboard.create()
 
-vstee = 0
-# todo: random player selection
-vplayer = 'A'
 
-while vstee < 8:
-    if vstee == 0:
-        vxp = rnd.randint(0,2)
-        vyp = rnd.randint(0,2)
-        vsymbol = 'X' if vplayer == 'A' else 'O'
-        newboard[vxp][vyp] = vsymbol
-        print(newboard.show())
+def mainGame():
+    newboard = brd.Board()
+    newboard.create()
+    vstee = 0
+    # todo: random player selection
+    vplayer = 1
+    while vstee <= 8:
+        print(vstee)
+        if vstee == 0:
+            newboard = smartmoves(isinit = 1, board = newboard, player = vplayer)
+            print(newboard.showFlat())
+            print(newboard.show())
+        else:
+            newboard = smartmoves(isinit = 0, board = newboard, player = vplayer)
+            print(newboard.showFlat())
+            print(newboard.show())
+        vplayer = vplayer*(-1)
+        vstee += 1
+
+
+def smartmoves(isinit, board, player):
+    if isinit == 1:
+        # vxp = rnd.randint(0,2)
+        # vyp = rnd.randint(0,2)
+        # board[vxp][vyp] = player
+        board[0][1] = player
     else:
-        print("Tu bedzie else")
-    print(vstee)
-    vstee += 1
+        opponent = -1*player
+        print(opponent)
+        print(board[0][1])
+        if (board[0][1] == opponent or 
+            board[2][1] == opponent or
+            board[1][0] == opponent or
+            board[1][2] == opponent):
+            board[1][1] = player
+    return board
 
-
+mainGame()
